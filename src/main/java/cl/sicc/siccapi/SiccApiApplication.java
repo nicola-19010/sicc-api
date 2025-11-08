95915c15a89c908f7ea2f08cbffab35ce1da60d2
Nicolás Pacheco <n.pacheco02@ufromail.cl>
2025-11-08 20:03:45 -0300

    initial commit

--- src/main/java/cl/sicc/siccapi/SiccApiApplication.java
+++ src/main/java/cl/sicc/siccapi/SiccApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiccApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SiccApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SiccApiApplication.class, args);
+    }
 
 }
